69742a44a35d911b685b79c47dee21059025e781
Nelson Moreno <nelson.moreno.cabanas@gmail.com>
2021-07-30 02:21:44 -0400

    lab6

--- Labs/Lab6/main.py
+++ Labs/Lab6/main.py
@@ -8,9 +8,11 @@ from tinydb.operations import set
 app = FastAPI(title="Lab 6")
 
 # aquí carguen el modelo guardado (con load de joblib) y
-model = None
+#model = load("C:/Users/bruno/OneDrive/Documentos/GitHub/LabProgra/Labs/Lab6/modelo.joblib")
+model = load("modelo.joblib")
+
 # el cliente de base de datos (con tinydb). Usen './db.json' como bbdd.
-db = None
+db = TinyDB("db.json")
 
 # Nota: En el caso que al guardar en la bbdd les salga una excepción del estilo JSONSerializable
 # conviertan el tipo de dato a uno más sencillo.
@@ -21,31 +23,48 @@ db = None
 # agregar más que esos.
 
 
+
 @app.post("/potabilidad/")
 async def predict_and_save(observation: Dict[str, float]):
-    # implementar 1. aquí
-    pass
+    valores = list(observation.values())
+    prediccion = model.predict([valores])
+    #Ahora insertamos el valor, con la fecha
+    hoy = datetime.now()
+    observation["Day"] = hoy.day
+    observation["Month"] = hoy.month
+    observation["Year"] = hoy.year
+    observation["Prediction"] = int(prediccion[0])
+
+    id = db.insert(observation)  # insertamos solo el diccionario, no la lista con el diccionario.
+
+# el número que imprime (121) indica id (indice) interno del documento recién insertado.
+    return {"Potabilidad": int(prediccion[0]) , "id": int(id)}
 
 
 @app.get("/potabilidad/")
 async def read_all():
     # implementar 2 aquí.
-    pass
+    return db.all()
 
 
 @app.get("/potabilidad_diaria/")
 async def read_by_day(day: int, month: int, year: int):
-    # implementar 3 aquí
-    pass
+    Mediciones = Query()
+    valores = db.search((Mediciones.Day == day) & (Mediciones.Month == month) & (Mediciones.Year == year))
+    return valores
 
 
 @app.put("/potabilidad/")
 async def update_by_day(day: int, month: int, year: int, new_prediction: int):
-    # implementar 4 aquí
-    pass
+    Mediciones = Query()
+    updated_elements = db.update(set("Prediction", new_prediction), (Mediciones.Day == day) & (Mediciones.Month == month) & (Mediciones.Year == year))
+    
+    return {"success": True, "updated_elements" : updated_elements}
 
 
 @app.delete("/potabilidad/")
 async def delete_by_day(day: int, month: int, year: int):
-    # implementar 5 aquí
-    pass
+    Mediciones = Query()
+    deleted_elements = db.remove((Mediciones.Day == day) & (Mediciones.Month == month) & (Mediciones.Year == year))
+    
+    return {"success": True, "deleted_elements" : deleted_elements}
